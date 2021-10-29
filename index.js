993c6fc3de69da97084a7656e0bd8a22059f5766
rezwanulislam114 <rezwanulislam114@gmail.com>
2021-10-29 08:36:02 +0600

    seting up with heroku

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient } = require('mongodb');
+const ObjectId = require('mongodb').ObjectId;
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware 
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tdrs3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+async function run() {
+    try {
+        await client.connect();
+        const database = client.db('trip');
+        const offerCollection = database.collection('offer')
+
+        // GET API 
+        app.get('/offers', async (req, res) => {
+            const cursor = offerCollection.find({});
+            const offers = await cursor.toArray();
+            res.send(offers);
+        })
+
+        // POST API 
+        app.post('/offers', async (req, res) => {
+            const offer = req.body;
+            const result = await offerCollection.insertOne(offer)
+            res.json(result);
+        })
+
+        // DELETE API 
+        app.delete('/offers/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await offerCollection.deleteOne(query);
+            res.json(result);
+        })
+    }
+    finally {
+        // await client.close()
+    }
+}
+run().catch(console.dir)
+
+// initialize database checking 
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
