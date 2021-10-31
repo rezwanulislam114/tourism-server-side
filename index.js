7ab2967e46d60e054c02ee4f9a0efb9b5f1b6c1a
rezwanulislam114 <rezwanulislam114@gmail.com>
2021-10-31 21:28:54 +0600

    cart delete

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ async function run() {
             res.send(result)
         })
 
-        app.delete('/offers/:id', async (req, res) => {
+        app.delete('/cart/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const result = await cartCollection.deleteOne(query);
